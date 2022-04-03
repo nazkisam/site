859a2900646da7bd078f46b84b518e32e44dec36
nazkisam <54238676+nazkisam@users.noreply.github.com>
2022-04-03 13:52:57 +0530

    cc

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// const parentcontainer=ducement.queryselector('.card-text');
-// parentcontainer.addeventlistener('click',event=>{
-//     const current = event.target;
-
-//     const isreadmorebtn = current.classname.includes('btn');
-//     if(!isbtn) return;
-//     const currenttext=event.target.parentnode.queryselector('.read-more-text');
-//     currenttext.classlist.toggle('read-more-text--show');
-//     current.textcontent = current.textcontent.includes('read more')?
-//     "read less...": "read more...";
-// })
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }

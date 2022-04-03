9d332556183e152f6dee4acd2f99947d8631577e
nazkisam <54238676+nazkisam@users.noreply.github.com>
2022-04-03 12:49:01 +0530

    kjfsnv,

---
+++ script.js
@@ -0,0 +1,11 @@
+// const parentcontainer=ducement.queryselector('.card-text');
+// parentcontainer.addeventlistener('click',event=>{
+//     const current = event.target;
+
+//     const isreadmorebtn = current.classname.includes('btn');
+//     if(!isbtn) return;
+//     const currenttext=event.target.parentnode.queryselector('.read-more-text');
+//     currenttext.classlist.toggle('read-more-text--show');
+//     current.textcontent = current.textcontent.includes('read more')?
+//     "read less...": "read more...";
+// })
